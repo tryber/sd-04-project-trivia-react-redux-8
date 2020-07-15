797e6f4846af700e69079a1fa8538d87d714702b
binhus <eng.robsonwilli@gmail.com>
2020-07-15 19:03:08 -0300

    Adding Header and game screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import GameScreen from './components/game_screen';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
+        <img src={logo} className="App-logo" alt="logo" />{' '}
+        {/* FAZER COMPONENTE PARA ESTA IMAGEM */}
+        <Link to="/game">
+          <p>SUA VEZ</p>
+        </Link>
       </header>
+      <Switch>
+        <Route path="/game" component={GameScreen} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/game_screen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from './header';
+
+const GameScreen = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <p data-testid="question-category">Categoria</p>
+        <p data-testid="question-text">Pergunta</p>
+      </div>
+      <div>
+        <div>
+          {<p>Resposta 1</p> /*Necessário fazer componente para a Pergunta} */}
+        </div>
+        <div>
+          <p>Resposta 2</p>
+        </div>
+        <div>
+          <p>Resposta 3</p>
+        </div>
+        <div>
+          <p>Resposta 4</p>
+        </div>
+      </div>
+      <button>Próxima Pergunta</button>
+    </div>
+  );
+};
+
+export default GameScreen;
