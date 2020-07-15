5976a47d6831fff2734f3ea253f2c1775d280099
binhus <eng.robsonwilli@gmail.com>
2020-07-15 19:19:46 -0300

    CC issues

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <div>
-      <h3 data-testid="header-player-name">Nome</h3>
-      <p data-testid="header-score">Score</p>
-      <img
-        data-testid="header-profile-picture"
-        src="https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3"
-        alt="Profile-Photo"
-        /* um dia será a imagem que está no state, obtida via API */
-      />
-    </div>
-  );
-};
+const Header = () => (
+  <div>
+    <h3 data-testid="header-player-name">Nome</h3>
+    <p data-testid="header-score">Score</p>
+    <img
+      data-testid="header-profile-picture"
+      src="https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3"
+      alt="Profile"
+      /* um dia será a imagem que está no state, obtida via API */
+    />
+  </div>
+);
 
 export default Header;
