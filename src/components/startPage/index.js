a9347fae8c686b8ee68da2bd4e887533ab111914
binhus <eng.robsonwilli@gmail.com>
2020-07-16 15:24:40 -0300

    Adding start page to the game

---
+++ src/components/startPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../trivia.png';
+
+export const StartPage = () => (
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <Link to="/login">
+      <p>Login</p>
+    </Link>
+  </div>
+);
