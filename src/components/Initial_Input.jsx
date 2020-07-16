d3d35fccb062e4156990c85afc569ad6921c0a43
samueap <samueap@gmail.com>
2020-07-16 05:54:29 -0300

    initial_sreen_login

---
+++ src/components/Initial_Input.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Login = () => {
+  const email = '';
+  const name = ''
+;    return (
+      <div>
+        <div>
+          <input
+            data-testid="input-gravatar-email"
+            className="box"
+            type="email"
+            value={email}
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <input
+              data-testid="input-player-name"
+              className="box" type="text"
+              value={name}
+              placeholder="Nome Jogador"
+            />
+        </div>
+        <div>
+          buttonDisabled() (
+            return (
+             <button
+                className="buttonPlay"
+                type="button"
+                data-testid="btn-play"
+                disabled
+              >Jogar
+             </button>
+            );
+          )
+        </div>
+      </div>
+    );
+};
+
+export default Login;
