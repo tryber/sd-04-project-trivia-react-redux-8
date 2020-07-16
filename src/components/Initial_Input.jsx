ff6ef0ecd7e1bf3f71224b2faabe977b5b1945fc
samueap <samueap@gmail.com>
2020-07-16 06:14:38 -0300

    try fix to CC

--- src/components/Initial_Input.jsx
+++ src/components/Initial_Input.jsx
@@ -1,42 +1,33 @@
-import React from "react";
+import React from 'react';
 
 const Login = () => {
   const email = '';
-  const name = ''
-;    return (
+  const name = '';
+  return (
+    <div>
       <div>
-        <div>
-          <input
-            data-testid="input-gravatar-email"
-            className="box"
-            type="email"
-            value={email}
-            placeholder="Email"
-          />
-        </div>
-        <div>
-          <input
-              data-testid="input-player-name"
-              className="box" type="text"
-              value={name}
-              placeholder="Nome Jogador"
-            />
-        </div>
-        <div>
-          buttonDisabled() (
-            return (
-             <button
-                className="buttonPlay"
-                type="button"
-                data-testid="btn-play"
-                disabled
-              >Jogar
-             </button>
-            );
-          )
-        </div>
+        <input
+          data-testid="input-gravatar-email" className="box" type="email" value={email}
+          placeholder="Email"
+        />
       </div>
-    );
+      <div>
+        <input
+            data-testid="input-player-name" className="box" type="text" value={name}
+            placeholder="Nome Jogador"
+          />
+      </div>
+      <div>
+        buttonDisabled() (
+          return (
+            <button
+              className="buttonPlay" type="button" data-testid="btn-play" disabled
+            >Jogar
+            </button>
+          );)
+      </div>
+    </div>
+  );
 };
 
 export default Login;
