dc93ef337481ad18aed239ac1f09afeb09c5ef6e
samueap <samueap@gmail.com>
2020-07-16 10:00:03 -0300

    teste com prettier

--- src/components/Initial_Input.jsx
+++ src/components/Initial_Input.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-  const email = '';
-  const name = '';
+  const email = "";
+  const name = "";
   return (
     <div>
       <div>
         <input
-          data-testid="input-gravatar-email" className="box" type="email" value={email}
+          data-testid="input-gravatar-email"
+          className="box"
+          type="email"
+          value={email}
           placeholder="Email"
         />
       </div>
       <div>
         <input
-          data-testid="input-player-name" className="box" type="text" value={name}
+          data-testid="input-player-name"
+          className="box"
+          type="text"
+          value={name}
           placeholder="Nome Jogador"
         />
       </div>
       <div>
-        buttonDisabled() (
-          return (
-            <button
-              className="buttonPlay" type="button" data-testid="btn-play" disabled
-            >Jogar
-            </button>
-          );)
+        buttonDisabled() ( return (
+        <button
+          className="buttonPlay"
+          type="button"
+          data-testid="btn-play"
+          disabled
+        >
+          Jogar
+        </button>
+        );)
       </div>
     </div>
   );
