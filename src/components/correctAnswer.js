4c522a616f6cedfb815e281cb8e6b549fc4e6eb0
binhus <eng.robsonwilli@gmail.com>
2020-07-21 00:09:56 -0300

    CC issues

--- src/components/correctAnswer.js
+++ src/components/correctAnswer.js
@@ -16,8 +16,8 @@ const CorrectAnswer = (props) => {
     <div>
       <button
         onClick={() => Click(data, index, score, timer, difficultyScore, difficulty, gotScore)}
-        data-testid="correct-answer"
-          >{resposta.correct_answer}
+        data-testid="correct-answer">
+          {resposta.correct_answer}
       </button>
     </div>
   );
--- src/components/wrongAnswer.js
+++ src/components/wrongAnswer.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -19,7 +19,7 @@ WrongAnswer.propTypes = {
   }).isRequired,
   i: PropTypes.number.isRequired,
   index: PropTypes.number.isRequired,
-}
+};
 
 const mapStateToProps = (state) => ({
   ...state.listaReducers,
